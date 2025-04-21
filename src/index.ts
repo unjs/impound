4da5e3f7522babddc55c6ef121b6ac0e9569f8da
Daniel Roe <daniel@roe.dev>
2025-04-21 16:08:32 +0100

    feat: support an array of `matchers` (#131)

--- src/index.ts
+++ src/index.ts
@@ -3,56 +3,62 @@ import { resolvePath } from 'mlly'
 import { isAbsolute, join, relative } from 'pathe'
 import { createUnplugin } from 'unplugin'
 
-export interface ImpoundOptions {
+export interface ImpoundMatcherOptions {
   /** An array of patterns of importers to apply the import protection rules to. */
   include?: Array<string | RegExp>
   /** An array of patterns of importers where the import protection rules explicitly do not apply. */
   exclude?: Array<string | RegExp>
-
-  cwd?: string
-
   /** Whether to throw an error or not. if set to `false`, an error will be logged to console instead. */
   error?: boolean
-
   /** An array of patterns to prevent being imported, along with an optional warning to display.  */
   patterns: [importPattern: string | RegExp, warning?: string][]
 }
 
+export interface ImpoundSharedOptions {
+  cwd?: string
+}
+
+export type ImpoundOptions = (ImpoundSharedOptions & ImpoundMatcherOptions) | (ImpoundSharedOptions & { matchers: ImpoundMatcherOptions[] })
+
 const RELATIVE_IMPORT_RE = /^\.\.?\//
 
-export const ImpoundPlugin = createUnplugin((options: ImpoundOptions) => {
-  const filter = createFilter(options.include, options.exclude, { resolve: options.cwd })
-  const proxy = resolvePath('mocked-exports/proxy', { url: import.meta.url })
+export const ImpoundPlugin = createUnplugin((globalOptions: ImpoundOptions) => {
+  const matchers = 'matchers' in globalOptions ? globalOptions.matchers : [globalOptions]
 
-  return {
-    name: 'impound',
-    enforce: 'pre',
-    resolveId(id, importer) {
-      if (!importer || !filter(importer)) {
-        return
-      }
+  return matchers.map((options) => {
+    const filter = createFilter(options.include, options.exclude, { resolve: globalOptions.cwd })
+    const proxy = resolvePath('mocked-exports/proxy', { url: import.meta.url })
 
-      if (RELATIVE_IMPORT_RE.test(id)) {
-        id = join(importer, '..', id)
-      }
-
-      if (isAbsolute(id) && options.cwd) {
-        id = relative(options.cwd, id)
-      }
-
-      let matched = false
-
-      const logError = options.error === false ? console.error : this.error.bind(this)
-      for (const [pattern, warning] of options.patterns) {
-        const usesImport = pattern instanceof RegExp ? pattern.test(id) : pattern === id
-        if (usesImport) {
-          const relativeImporter = isAbsolute(importer) && options.cwd ? relative(options.cwd, importer) : importer
-          logError(`${warning || 'Invalid import'} [importing \`${id}\` from \`${relativeImporter}\`]`)
-          matched = true
+    return {
+      name: 'impound',
+      enforce: 'pre',
+      resolveId(id, importer) {
+        if (!importer || !filter(importer)) {
+          return
         }
-      }
 
-      return matched ? proxy : null
-    },
-  }
+        if (RELATIVE_IMPORT_RE.test(id)) {
+          id = join(importer, '..', id)
+        }
+
+        if (isAbsolute(id) && globalOptions.cwd) {
+          id = relative(globalOptions.cwd, id)
+        }
+
+        let matched = false
+
+        const logError = options.error === false ? console.error : this.error.bind(this)
+        for (const [pattern, warning] of options.patterns) {
+          const usesImport = pattern instanceof RegExp ? pattern.test(id) : pattern === id
+          if (usesImport) {
+            const relativeImporter = isAbsolute(importer) && globalOptions.cwd ? relative(globalOptions.cwd, importer) : importer
+            logError(`${warning || 'Invalid import'} [importing \`${id}\` from \`${relativeImporter}\`]`)
+            matched = true
+          }
+        }
+
+        return matched ? proxy : null
+      },
+    }
+  })
 })
